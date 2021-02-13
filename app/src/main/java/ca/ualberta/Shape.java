51cc11329dc5759814698436069db124cba0a06b
R <mnaveed@ualberta.ca>
2021-02-12 18:42:40 -0700

    added by raamish

--- app/src/main/java/ca/ualberta/Shape.java
+++ app/src/main/java/ca/ualberta/Shape.java
@@ -1,7 +1,7 @@
 package ca.ualberta;
 
 public abstract class Shape {
-    String color = "MAGENTA";
+    String color = "rrr";
     private int x;
     private int y;
     public Shape(int x, int y) {
