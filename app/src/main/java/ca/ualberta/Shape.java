5d43b996afd2135c91b4e38437b998cd031f5fdb
Danh Nguyen <65102457+danny-cpp@users.noreply.github.com>
2021-02-12 20:53:03 -0500

    Update Shape.java

--- app/src/main/java/ca/ualberta/Shape.java
+++ app/src/main/java/ca/ualberta/Shape.java
@@ -1,7 +1,7 @@
 package ca.ualberta;
 
 public abstract class Shape {
-    String color = "blue";
+    String color = "Green";
     private int x;
     private int y;
     public Shape(int x, int y) {
