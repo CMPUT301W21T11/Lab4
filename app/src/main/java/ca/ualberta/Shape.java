3d4830d2d66b9385efb0ac74de132b444c741504
R <mnaveed@ualberta.ca>
2021-02-12 18:50:59 -0700

    added by raamish

--- app/src/main/java/ca/ualberta/Shape.java
+++ app/src/main/java/ca/ualberta/Shape.java
@@ -1,7 +1,7 @@
 package ca.ualberta;
 
 public abstract class Shape {
-    String color = "Orange";
+    String color = "blue";
     private int x;
     private int y;
     public Shape(int x, int y) {
